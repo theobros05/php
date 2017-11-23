d1b1ef3ec78c81bd4e3e9d5a836370149e559b1b
theobros05 <31876242+theobros05@users.noreply.github.com>
2017-11-23 17:22:09 +0530

    code in text

---
+++ iframe.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<iframe height="300px" width="100%" src="registration.html" name="iframe_a"></iframe>
+
+<p><a href="index.html" target="iframe_a">Redirecting to login page</a></p>
+
+<p>When the target of a link matches the name of an iframe, the link will open in the iframe.</p>
+
+</body>
+</html>
